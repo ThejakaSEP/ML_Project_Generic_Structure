228c1ba24f5d7562f34e9ada77f6379a0ca39706
ThejakaSEP <thejaka.mahaulpatha@gmail.com>
2023-03-26 23:58:29 -0400

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -12,6 +12,7 @@ def error_message_detail(error,error_detail:sys):
     
 
 class CustomException(Exception):
+
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -20,5 +21,11 @@ class CustomException(Exception):
         return self.error_message
     
 
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Devide by Zero")
+#         raise CustomException(e,sys)
 
         
